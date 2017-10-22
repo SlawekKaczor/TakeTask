d82c3344a1c8cbdc67a52f31838f2d004d4ca9f3
SlawekKaczor <slawomir.kaczor@gmail.com>
2017-10-22 23:25:15 +0200

    Framework for TakeTask application

--- TakeTask/Program/OptionPrinter.cs
+++ TakeTask/Program/OptionPrinter.cs
@@ -6,6 +6,13 @@ namespace TakeTask
 {
     public static class OptionPrinter
     {
+        public const string ADD_TASK_COMMAND_NAME = "a";
+        public const string DISPLAY_ALL_TASKS_COMMAND_NAME = "d";
+        public const string REMOVE_TASK_COMMAND_NAME = "r";
+        public const string LOAD_TASK_COMMAND_NAME = "l";
+        public const string SAVE_TASK_COMMAND_NAME = "s";
+        public const string EXIT_COMMAND_NAME = "e";
+
         public static void PrintWelcomeMessage()
         {
             Console.WriteLine("Welcome to TakeTask 1.0");
@@ -14,12 +21,17 @@ namespace TakeTask
         public static void PrintOptions()
         {
             Console.WriteLine("Please choose one from the following options: ");
-            Console.WriteLine("\"a\" to add a new new task to task list");
-            Console.WriteLine("\"d\" to display all tasks from the task list");
-            Console.WriteLine("\"r\" to remove task from the task list");
-            Console.WriteLine("\"l\" to load tasks from the file or a database");
-            Console.WriteLine("\"s\" to save tasks to the file or a database");
-            Console.WriteLine("\"e\" to exit the program");
+            Console.WriteLine($"\"{ADD_TASK_COMMAND_NAME}\" " +
+                $" to add a new new task to task list");
+            Console.WriteLine($"\"{DISPLAY_ALL_TASKS_COMMAND_NAME}\" " +
+                $" to display all tasks from the task list");
+            Console.WriteLine($"\"{REMOVE_TASK_COMMAND_NAME}\" " +
+                $" to remove task from the task list");
+            Console.WriteLine($"\"{LOAD_TASK_COMMAND_NAME}\" " +
+                $" to load tasks from the file or a database");
+            Console.WriteLine($"\"{SAVE_TASK_COMMAND_NAME}\" " +
+                $" to save tasks to the file or a database");
+            Console.WriteLine($"\"{EXIT_COMMAND_NAME}\" to exit the program");
         }
 
     }
--- TakeTask/Program/OptionReader.cs
+++ TakeTask/Program/OptionReader.cs
@@ -4,38 +4,32 @@ namespace TakeTask
 {
     public static class OptionReader
     {
-        public const string ADD_TASK_COMMAND_NAME = "a";
-        public const string DISPLAY_ALL_TASKS_COMMAND_NAME = "d";
-        public const string REMOVE_TASK_COMMAND_NAME = "r";
-        public const string LOAD_TASK_COMMAND_NAME = "l";
-        public const string SAVE_TASK_COMMAND_NAME = "s";
-        public const string EXIT_COMMAND_NAME = "e";
-
+        
         public static Option GetOption()
         {
             Console.Write("Please choose your command: ");
             string command = Console.ReadLine();
-            if (command.Equals(ADD_TASK_COMMAND_NAME)) 
+            if (command.Equals("a"))
             {
                 return Option.AddTask;
             } 
-            else if (command.Equals(DISPLAY_ALL_TASKS_COMMAND_NAME))
+            else if (command.Equals("d"))
             {
                 return Option.DisplayAllTasks;
             }
-            else if (command.Equals(REMOVE_TASK_COMMAND_NAME))
+            else if (command.Equals("r"))
             {
                 return Option.RemoveTask;
             }
-            else if (command.Equals(LOAD_TASK_COMMAND_NAME))
+            else if (command.Equals("l"))
             {
                 return Option.LoadAllTasks;
             }
-            else if (command.Equals(SAVE_TASK_COMMAND_NAME))
+            else if (command.Equals("s"))
             {
                 return Option.SaveTask;
             }
-            else if (command.Equals(EXIT_COMMAND_NAME))
+            else if (command.Equals("e"))
             {
                 return Option.ExitProgram;
             }
